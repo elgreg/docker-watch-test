f9f17fad82196a53d7ac6fc30a3118ad799556c3
Greg Lavallee <greg.lavallee@slate.com>
2016-07-30 23:09:46 -0400

    Change to add Gruntfilw watch
    CPU during watch ~0.24% via docker stats
    CPU during watch ~4.50% via activity monitor
    
    CPU with no-watch ~0.00% via docker stats
    CPU with no-watch ~2.80% via acttivity monitor

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,7 +21,7 @@ module.exports = function(grunt) {
       },
     },
     watch: {
-      files: ['Gruntfile.js', 'src/**'],
+      files: ['Gruntfile.js', 'src/*'],
       tasks: ['jshint:all', 'uglify'],
     },
   });
